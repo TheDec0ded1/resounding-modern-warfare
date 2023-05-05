d87e5d0400dfac2277f204d809bfa5f38b587636
TheDec0ded1 <126051521+TheDec0ded1@users.noreply.github.com>
2023-05-05 17:29:17 -0500

    Add files via upload

---
+++ src/main/java/com/vicmatskiv/mw/models/AKSUHandguard.java
@@ -0,0 +1,447 @@
+package com.vicmatskiv.mw.models;
+
+import com.vicmatskiv.weaponlib.ModelWithAttachments;
+
+import net.minecraft.client.model.ModelBox;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.entity.Entity;
+
+// Made with Blockbench
+// Paste this code into your mod.
+// Make sure to generate all required imports
+
+public class custom_model extends ModelBase {
+	private final ModelRenderer handguard;
+	private final ModelRenderer gun232;
+	private final ModelRenderer gun231;
+	private final ModelRenderer gun230;
+	private final ModelRenderer gun229;
+	private final ModelRenderer gun227;
+	private final ModelRenderer gun228;
+	private final ModelRenderer gun226;
+	private final ModelRenderer gun225;
+	private final ModelRenderer gun224;
+	private final ModelRenderer gun223;
+	private final ModelRenderer gun222;
+	private final ModelRenderer gun221;
+	private final ModelRenderer gun220;
+	private final ModelRenderer gun219;
+	private final ModelRenderer gun218;
+	private final ModelRenderer gun217;
+	private final ModelRenderer gun216;
+	private final ModelRenderer gun215;
+	private final ModelRenderer gun214;
+	private final ModelRenderer gun213;
+	private final ModelRenderer gun212;
+	private final ModelRenderer gun211;
+	private final ModelRenderer gun210;
+	private final ModelRenderer gun209;
+	private final ModelRenderer gun193;
+	private final ModelRenderer gun192;
+	private final ModelRenderer gun191;
+	private final ModelRenderer gun190;
+	private final ModelRenderer gun189;
+	private final ModelRenderer gun188;
+	private final ModelRenderer gun187;
+	private final ModelRenderer gun186;
+	private final ModelRenderer gun185;
+	private final ModelRenderer gun184;
+	private final ModelRenderer gun183;
+	private final ModelRenderer gun181;
+	private final ModelRenderer gun180;
+	private final ModelRenderer gun179;
+	private final ModelRenderer gun178;
+	private final ModelRenderer gun177;
+	private final ModelRenderer gun176;
+	private final ModelRenderer gun175;
+	private final ModelRenderer gun174;
+	private final ModelRenderer gun173;
+	private final ModelRenderer gun172;
+	private final ModelRenderer gun171;
+	private final ModelRenderer gun170;
+	private final ModelRenderer gun169;
+	private final ModelRenderer gun168;
+	private final ModelRenderer gun164;
+	private final ModelRenderer gun162;
+	private final ModelRenderer gun161;
+	private final ModelRenderer gun160;
+	private final ModelRenderer gun159;
+	private final ModelRenderer gun158;
+	private final ModelRenderer gun157;
+	private final ModelRenderer gun156;
+	private final ModelRenderer gun155;
+	private final ModelRenderer gun154;
+	private final ModelRenderer gun153;
+	private final ModelRenderer gun152;
+	private final ModelRenderer gun151;
+	private final ModelRenderer gun150;
+	private final ModelRenderer gun149;
+	private final ModelRenderer gun148;
+
+	public custom_model() {
+		textureWidth = 200;
+		textureHeight = 200;
+
+		handguard = new ModelRenderer(this);
+		handguard.setRotationPoint(0.0F, 24.0F, 0.0F);
+
+		gun232 = new ModelRenderer(this);
+		gun232.setRotationPoint(0.0F, -33.0F, -53.5F);
+		setRotationAngle(gun232, 0.0F, 0.0F, -2.3794F);
+		handguard.addChild(gun232);
+		gun232.cubeList.add(new ModelBox(gun232, 115, 53, 0.0F, 0.0F, -0.001F, 1, 1, 14, 0.0F, false));
+
+		gun231 = new ModelRenderer(this);
+		gun231.setRotationPoint(1.0F, -34.0F, -53.5F);
+		setRotationAngle(gun231, 0.0F, 0.0F, 2.3794F);
+		handguard.addChild(gun231);
+		gun231.cubeList.add(new ModelBox(gun231, 141, 14, 0.0F, 0.0F, 0.0F, 1, 1, 11, 0.0F, false));
+
+		gun230 = new ModelRenderer(this);
+		gun230.setRotationPoint(-4.0F, -34.0F, -53.5F);
+		setRotationAngle(gun230, 0.0F, 0.0F, -0.7808F);
+		handguard.addChild(gun230);
+		gun230.cubeList.add(new ModelBox(gun230, 117, 92, 0.0F, 0.0F, 0.0F, 1, 1, 8, 0.0F, false));
+
+		gun229 = new ModelRenderer(this);
+		gun229.setRotationPoint(-3.0F, -33.0F, -53.5F);
+		setRotationAngle(gun229, 0.0F, 0.0F, -2.3423F);
+		handguard.addChild(gun229);
+		gun229.cubeList.add(new ModelBox(gun229, 64, 140, 0.0F, 0.0F, -0.001F, 1, 1, 11, 0.0F, false));
+
+		gun227 = new ModelRenderer(this);
+		gun227.setRotationPoint(-2.9F, -39.7F, -54.0F);
+		setRotationAngle(gun227, 0.0F, 0.0F, 0.8179F);
+		handguard.addChild(gun227);
+		gun227.cubeList.add(new ModelBox(gun227, 63, 59, 0.0F, 0.0F, 0.001F, 1, 1, 1, 0.0F, false));
+
+		gun228 = new ModelRenderer(this);
+		gun228.setRotationPoint(-3.9F, -38.7F, -54.0F);
+		setRotationAngle(gun228, 0.0F, 0.0F, -0.7808F);
+		handguard.addChild(gun228);
+		gun228.cubeList.add(new ModelBox(gun228, 63, 57, 0.0F, 0.0F, 0.0F, 1, 1, 1, 0.0F, false));
+
+		gun226 = new ModelRenderer(this);
+		gun226.setRotationPoint(0.9F, -38.7F, -54.0F);
+		setRotationAngle(gun226, 0.0F, 0.0F, 2.3794F);
+		handguard.addChild(gun226);
+		gun226.cubeList.add(new ModelBox(gun226, 0, 63, 0.0F, 0.0F, 0.0F, 1, 1, 1, 0.0F, false));
+
+		gun225 = new ModelRenderer(this);
+		gun225.setRotationPoint(-0.1F, -39.7F, -54.0F);
+		setRotationAngle(gun225, 0.0F, 0.0F, 0.7808F);
+		handguard.addChild(gun225);
+		gun225.cubeList.add(new ModelBox(gun225, 61, 45, 0.0F, 0.0F, 0.001F, 1, 1, 1, 0.0F, false));
+
+		gun224 = new ModelRenderer(this);
+		gun224.setRotationPoint(0.9F, -34.7F, -54.0F);
+		setRotationAngle(gun224, 0.0F, 0.0F, 2.0448F);
+		handguard.addChild(gun224);
+		gun224.cubeList.add(new ModelBox(gun224, 58, 51, 0.0F, 0.0F, 0.0F, 1, 1, 1, 0.0F, false));
+
+		gun223 = new ModelRenderer(this);
+		gun223.setRotationPoint(-0.1F, -33.2F, -54.0F);
+		setRotationAngle(gun223, 0.0F, 0.0F, -2.4363F);
+		handguard.addChild(gun223);
+		gun223.cubeList.add(new ModelBox(gun223, 58, 49, 0.0F, 0.0F, 0.001F, 1, 1, 1, 0.0F, false));
+
+		gun222 = new ModelRenderer(this);
+		gun222.setRotationPoint(-2.9F, -33.2F, -54.0F);
+		setRotationAngle(gun222, 0.0F, 0.0F, -2.2679F);
+		handguard.addChild(gun222);
+		gun222.cubeList.add(new ModelBox(gun222, 10, 56, 0.0F, 0.0F, 0.001F, 1, 1, 1, 0.0F, false));
+
+		gun221 = new ModelRenderer(this);
+		gun221.setRotationPoint(-3.9F, -34.7F, -54.0F);
+		setRotationAngle(gun221, 0.0F, 0.0F, -0.4833F);
+		handguard.addChild(gun221);
+		gun221.cubeList.add(new ModelBox(gun221, 0, 56, 0.0F, 0.0F, 0.0F, 1, 1, 1, 0.0F, false));
+
+		gun220 = new ModelRenderer(this);
+		gun220.setRotationPoint(-1.1F, -35.2F, -54.0F);
+		handguard.addChild(gun220);
+		gun220.cubeList.add(new ModelBox(gun220, 27, 19, 0.0F, -0.001F, -0.002F, 1, 2, 1, 0.0F, false));
+
+		gun219 = new ModelRenderer(this);
+		gun219.setRotationPoint(-2.9F, -35.2F, -54.0F);
+		handguard.addChild(gun219);
+		gun219.cubeList.add(new ModelBox(gun219, 32, 34, 0.0F, 0.0F, -0.001F, 2, 2, 1, 0.0F, false));
+
+		gun218 = new ModelRenderer(this);
+		gun218.setRotationPoint(-1.1F, -38.7F, -54.0F);
+		handguard.addChild(gun218);
+		gun218.cubeList.add(new ModelBox(gun218, 12, 31, 0.0F, 0.0F, -0.003F, 2, 4, 1, 0.0F, false));
+
+		gun217 = new ModelRenderer(this);
+		gun217.setRotationPoint(-3.9F, -38.7F, -54.0F);
+		handguard.addChild(gun217);
+		gun217.cubeList.add(new ModelBox(gun217, 7, 63, 0.0F, 0.0F, -0.002F, 3, 4, 1, 0.0F, false));
+
+		gun216 = new ModelRenderer(this);
+		gun216.setRotationPoint(-1.1F, -39.7F, -54.0F);
+		handguard.addChild(gun216);
+		gun216.cubeList.add(new ModelBox(gun216, 55, 0, 0.0F, -0.001F, -0.002F, 1, 1, 1, 0.0F, false));
+
+		gun215 = new ModelRenderer(this);
+		gun215.setRotationPoint(-2.9F, -39.7F, -54.0F);
+		handguard.addChild(gun215);
+		gun215.cubeList.add(new ModelBox(gun215, 32, 16, 0.0F, 0.0F, -0.001F, 2, 1, 1, 0.0F, false));
+
+		gun214 = new ModelRenderer(this);
+		gun214.setRotationPoint(-3.0F, -37.5F, -51.5F);
+		handguard.addChild(gun214);
+		gun214.cubeList.add(new ModelBox(gun214, 46, 140, 0.0F, 0.0F, 0.0F, 3, 1, 12, 0.0F, false));
+
+		gun213 = new ModelRenderer(this);
+		gun213.setRotationPoint(-3.5F, -37.5F, -41.5F);
+		handguard.addChild(gun213);
+		gun213.cubeList.add(new ModelBox(gun213, 53, 77, 0.0F, 0.0F, 0.0F, 4, 1, 1, 0.0F, false));
+
+		gun212 = new ModelRenderer(this);
+		gun212.setRotationPoint(-3.5F, -37.5F, -47.5F);
+		handguard.addChild(gun212);
+		gun212.cubeList.add(new ModelBox(gun212, 68, 23, 0.0F, 0.0F, 0.0F, 4, 1, 2, 0.0F, false));
+
+		gun211 = new ModelRenderer(this);
+		gun211.setRotationPoint(-3.5F, -37.5F, -53.5F);
+		handguard.addChild(gun211);
+		gun211.cubeList.add(new ModelBox(gun211, 68, 20, 0.0F, 0.0F, 0.0F, 4, 1, 2, 0.0F, false));
+
+		gun210 = new ModelRenderer(this);
+		gun210.setRotationPoint(-3.5F, -37.0F, -53.5F);
+		handguard.addChild(gun210);
+		gun210.cubeList.add(new ModelBox(gun210, 47, 14, 0.0F, 0.1F, 0.0F, 4, 1, 13, 0.0F, false));
+
+		gun209 = new ModelRenderer(this);
+		gun209.setRotationPoint(-3.5F, -38.1F, -53.5F);
+		handguard.addChild(gun209);
+		gun209.cubeList.add(new ModelBox(gun209, 26, 113, 0.0F, -0.2F, 0.0F, 4, 1, 12, 0.0F, false));
+
+		gun193 = new ModelRenderer(this);
+		gun193.setRotationPoint(-3.0F, -31.5F, -39.5F);
+		handguard.addChild(gun193);
+		gun193.cubeList.add(new ModelBox(gun193, 83, 25, 0.0F, 0.0F, 0.0F, 3, 1, 1, 0.0F, false));
+
+		gun192 = new ModelRenderer(this);
+		gun192.setRotationPoint(-3.0F, -30.5F, -39.5F);
+		setRotationAngle(gun192, 2.6025F, 0.0F, 0.0F);
+		handguard.addChild(gun192);
+		gun192.cubeList.add(new ModelBox(gun192, 97, 124, -0.001F, 0.0F, 0.0F, 3, 2, 5, 0.0F, false));
+
+		gun191 = new ModelRenderer(this);
+		gun191.setRotationPoint(-4.0F, -33.0F, -37.5F);
+		handguard.addChild(gun191);
+		gun191.cubeList.add(new ModelBox(gun191, 41, 64, 0.0F, 0.0F, 0.0F, 5, 2, 1, 0.0F, false));
+
+		gun190 = new ModelRenderer(this);
+		gun190.setRotationPoint(-4.0F, -30.5F, -37.5F);
+		setRotationAngle(gun190, 2.1192F, 0.0F, 0.0F);
+		handguard.addChild(gun190);
+		gun190.cubeList.add(new ModelBox(gun190, 41, 77, -0.001F, 0.0F, 0.0F, 5, 1, 1, 0.0F, false));
+
+		gun189 = new ModelRenderer(this);
+		gun189.setRotationPoint(-4.0F, -30.5F, -38.5F);
+		setRotationAngle(gun189, 2.6025F, 0.0F, 0.0F);
+		handguard.addChild(gun189);
+		gun189.cubeList.add(new ModelBox(gun189, 117, 79, -0.001F, 0.0F, 0.0F, 5, 2, 5, 0.0F, false));
+
+		gun188 = new ModelRenderer(this);
+		gun188.setRotationPoint(-4.0F, -33.5F, -38.5F);
+		handguard.addChild(gun188);
+		gun188.cubeList.add(new ModelBox(gun188, 47, 23, 0.001F, 0.0F, 0.0F, 5, 3, 1, 0.0F, false));
+
+		gun187 = new ModelRenderer(this);
+		gun187.setRotationPoint(1.0F, -38.9F, -53.5F);
+		setRotationAngle(gun187, 0.0F, 0.0F, 2.3051F);
+		handguard.addChild(gun187);
+		gun187.cubeList.add(new ModelBox(gun187, 140, 73, 0.0F, 0.0F, -0.002F, 1, 1, 13, 0.0F, false));
+
+		gun186 = new ModelRenderer(this);
+		gun186.setRotationPoint(0.0F, -39.8F, -53.5F);
+		setRotationAngle(gun186, 0.0F, 0.0F, 0.7436F);
+		handguard.addChild(gun186);
+		gun186.cubeList.add(new ModelBox(gun186, 88, 110, 0.0F, 0.0F, -0.001F, 1, 1, 13, 0.0F, false));
+
+		gun185 = new ModelRenderer(this);
+		gun185.setRotationPoint(-3.0F, -39.8F, -53.5F);
+		setRotationAngle(gun185, 0.0F, 0.0F, 0.8179F);
+		handguard.addChild(gun185);
+		gun185.cubeList.add(new ModelBox(gun185, 83, 14, 0.0F, 0.0F, -0.001F, 1, 1, 13, 0.0F, false));
+
+		gun184 = new ModelRenderer(this);
+		gun184.setRotationPoint(-4.0F, -38.9F, -53.5F);
+		setRotationAngle(gun184, 0.0F, 0.0F, -0.7064F);
+		handguard.addChild(gun184);
+		gun184.cubeList.add(new ModelBox(gun184, 83, 0, 0.0F, 0.0F, -0.002F, 1, 1, 13, 0.0F, false));
+
+		gun183 = new ModelRenderer(this);
+		gun183.setRotationPoint(-3.0F, -39.8F, -53.5F);
+		setRotationAngle(gun183, 0.0175F, 0.0F, 0.0F);
+		handguard.addChild(gun183);
+		gun183.cubeList.add(new ModelBox(gun183, 79, 35, 0.0F, 0.0F, 0.0F, 3, 1, 13, 0.0F, false));
+
+		gun181 = new ModelRenderer(this);
+		gun181.setRotationPoint(-4.0F, -37.0F, -51.5F);
+		setRotationAngle(gun181, -0.7808F, 0.0F, 0.0F);
+		handguard.addChild(gun181);
+
+		gun180 = new ModelRenderer(this);
+		gun180.setRotationPoint(-4.0F, -37.0F, -45.5F);
+		setRotationAngle(gun180, -0.7808F, 0.0F, 0.0F);
+		handguard.addChild(gun180);
+
+		gun179 = new ModelRenderer(this);
+		gun179.setRotationPoint(-4.0F, -37.0F, -41.5F);
+		setRotationAngle(gun179, -0.7436F, 0.0F, 0.0F);
+		handguard.addChild(gun179);
+
+		gun178 = new ModelRenderer(this);
+		gun178.setRotationPoint(-4.0F, -37.0F, -47.5F);
+		setRotationAngle(gun178, -0.7436F, 0.0F, 0.0F);
+		handguard.addChild(gun178);
+
+		gun177 = new ModelRenderer(this);
+		gun177.setRotationPoint(-4.0F, -37.2F, -51.5F);
+		setRotationAngle(gun177, 2.3794F, 0.0F, 0.0F);
+		handguard.addChild(gun177);
+
+		gun176 = new ModelRenderer(this);
+		gun176.setRotationPoint(-4.0F, -37.2F, -45.5F);
+		setRotationAngle(gun176, 2.3794F, 0.0F, 0.0F);
+		handguard.addChild(gun176);
+
+		gun175 = new ModelRenderer(this);
+		gun175.setRotationPoint(-4.0F, -37.2F, -47.5F);
+		setRotationAngle(gun175, 2.3794F, 0.0F, 0.0F);
+		handguard.addChild(gun175);
+
+		gun174 = new ModelRenderer(this);
+		gun174.setRotationPoint(-4.0F, -37.2F, -41.5F);
+		setRotationAngle(gun174, 2.3794F, 0.0F, 0.0F);
+		handguard.addChild(gun174);
+
+		gun173 = new ModelRenderer(this);
+		gun173.setRotationPoint(-4.0F, -38.8F, -41.5F);
+		handguard.addChild(gun173);
+
+		gun172 = new ModelRenderer(this);
+		gun172.setRotationPoint(-4.0F, -38.2F, -41.5F);
+		handguard.addChild(gun172);
+		gun172.cubeList.add(new ModelBox(gun172, 41, 53, 0.001F, 0.0F, -0.001F, 5, 1, 1, 0.0F, false));
+
+		gun171 = new ModelRenderer(this);
+		gun171.setRotationPoint(-4.0F, -38.2F, -47.5F);
+		handguard.addChild(gun171);
+		gun171.cubeList.add(new ModelBox(gun171, 100, 94, 0.002F, 0.0F, -4.0F, 5, 1, 10, 0.0F, false));
+
+		gun170 = new ModelRenderer(this);
+		gun170.setRotationPoint(-4.0F, -38.2F, -53.5F);
+		handguard.addChild(gun170);
+		gun170.cubeList.add(new ModelBox(gun170, 58, 35, 0.002F, 0.0F, 0.0F, 5, 1, 2, 0.0F, false));
+
+		gun169 = new ModelRenderer(this);
+		gun169.setRotationPoint(-4.0F, -37.0F, -47.5F);
+		handguard.addChild(gun169);
+		gun169.cubeList.add(new ModelBox(gun169, 102, 93, 0.0F, 0.0F, -4.0F, 5, 1, 10, 0.0F, false));
+
+		gun168 = new ModelRenderer(this);
+		gun168.setRotationPoint(-4.0F, -37.0F, -53.5F);
+		handguard.addChild(gun168);
+		gun168.cubeList.add(new ModelBox(gun168, 23, 50, 0.0F, 0.0F, 0.0F, 5, 1, 2, 0.0F, false));
+
+		gun164 = new ModelRenderer(this);
+		gun164.setRotationPoint(-4.0F, -38.7F, -53.5F);
+		handguard.addChild(gun164);
+		gun164.cubeList.add(new ModelBox(gun164, 41, 57, 0.001F, 0.0F, 0.0F, 5, 1, 12, 0.0F, false));
+
+		gun162 = new ModelRenderer(this);
+		gun162.setRotationPoint(-4.0F, -38.9F, -53.5F);
+		handguard.addChild(gun162);
+		gun162.cubeList.add(new ModelBox(gun162, 0, 40, 0.003F, 0.0F, 0.0F, 5, 1, 13, 0.0F, false));
+
+		gun161 = new ModelRenderer(this);
+		gun161.setRotationPoint(-4.0F, -34.8F, -48.5F);
+		handguard.addChild(gun161);
+		gun161.cubeList.add(new ModelBox(gun161, 0, 70, 0.0F, 0.0F, 0.0F, 5, 1, 7, 0.0F, false));
+
+		gun160 = new ModelRenderer(this);
+		gun160.setRotationPoint(-4.0F, -33.5F, -37.5F);
+		handguard.addChild(gun160);
+
+		gun159 = new ModelRenderer(this);
+		gun159.setRotationPoint(-3.0F, -33.0F, -53.5F);
+		setRotationAngle(gun159, 1.4871F, 0.0F, 0.0F);
+		handguard.addChild(gun159);
+		gun159.cubeList.add(new ModelBox(gun159, 91, 135, 0.0F, 0.0F, 0.0F, 3, 17, 1, 0.0F, false));
+
+		gun158 = new ModelRenderer(this);
+		gun158.setRotationPoint(-3.0F, -34.0F, -53.5F);
+		handguard.addChild(gun158);
+		gun158.cubeList.add(new ModelBox(gun158, 114, 135, 0.0F, 0.0F, 0.0F, 3, 1, 12, 0.0F, false));
+
+		gun157 = new ModelRenderer(this);
+		gun157.setRotationPoint(-4.0F, -34.0F, -53.5F);
+		setRotationAngle(gun157, 1.4871F, 0.0F, 0.0F);
+		handguard.addChild(gun157);
+		gun157.cubeList.add(new ModelBox(gun157, 0, 19, 0.002F, 0.0F, 0.0F, 5, 17, 1, 0.0F, false));
+
+		gun156 = new ModelRenderer(this);
+		gun156.setRotationPoint(-3.0F, -31.0F, -36.5F);
+		setRotationAngle(gun156, 0.0F, 0.0F, -2.4166F);
+		handguard.addChild(gun156);
+		gun156.cubeList.add(new ModelBox(gun156, 10, 54, 0.0F, 0.0F, 0.001F, 1, 1, 1, 0.0F, false));
+
+		gun155 = new ModelRenderer(this);
+		gun155.setRotationPoint(-3.9F, -31.9F, -36.5F);
+		setRotationAngle(gun155, 0.0F, 0.0F, -0.7064F);
+		handguard.addChild(gun155);
+		gun155.cubeList.add(new ModelBox(gun155, 0, 54, 0.0F, 0.0F, -0.001F, 1, 1, 1, 0.0F, false));
+
+		gun154 = new ModelRenderer(this);
+		gun154.setRotationPoint(0.9F, -31.9F, -36.5F);
+		setRotationAngle(gun154, 0.0F, 0.0F, 2.3794F);
+		handguard.addChild(gun154);
+		gun154.cubeList.add(new ModelBox(gun154, 35, 50, 0.0F, 0.0F, -0.001F, 1, 1, 1, 0.0F, false));
+
+		gun153 = new ModelRenderer(this);
+		gun153.setRotationPoint(0.0F, -31.0F, -36.5F);
+		setRotationAngle(gun153, 0.0F, 0.0F, -2.3423F);
+		handguard.addChild(gun153);
+		gun153.cubeList.add(new ModelBox(gun153, 47, 17, 0.0F, 0.0F, 0.001F, 1, 1, 1, 0.0F, false));
+
+		gun152 = new ModelRenderer(this);
+		gun152.setRotationPoint(-3.0F, -32.0F, -36.5F);
+		handguard.addChild(gun152);
+		gun152.cubeList.add(new ModelBox(gun152, 83, 11, 0.0F, 0.0F, -0.001F, 3, 1, 1, 0.0F, false));
+
+		gun151 = new ModelRenderer(this);
+		gun151.setRotationPoint(-3.9F, -36.9F, -36.5F);
+		handguard.addChild(gun151);
+		gun151.cubeList.add(new ModelBox(gun151, 58, 40, 0.0F, 0.0F, 0.0F, 1, 5, 1, 0.0F, false));
+
+		gun150 = new ModelRenderer(this);
+		gun150.setRotationPoint(-0.1F, -36.9F, -36.5F);
+		handguard.addChild(gun150);
+		gun150.cubeList.add(new ModelBox(gun150, 35, 53, 0.0F, 0.0F, 0.0F, 1, 5, 1, 0.0F, false));
+
+		gun149 = new ModelRenderer(this);
+		gun149.setRotationPoint(-4.0F, -36.5F, -53.5F);
+		handguard.addChild(gun149);
+		gun149.cubeList.add(new ModelBox(gun149, 47, 0, 0.001F, 0.0F, 0.0F, 5, 2, 12, 0.0F, false));
+
+		gun148 = new ModelRenderer(this);
+		gun148.setRotationPoint(-4.0F, -37.0F, -41.5F);
+		handguard.addChild(gun148);
+		gun148.cubeList.add(new ModelBox(gun148, 117, 70, 0.0F, 0.0F, 0.0F, 5, 4, 5, 0.0F, false));
+	}
+
+	@Override
+	public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) {
+		handguard.render(f5);
+	}
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
+}
